1af852661bf9357038a84758a29359920a8aab6d
Iryna <gresrin@gmail.com>
2022-09-17 12:55:53 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./App.css";
 
+import Search from "./Search";
+
    export default function Weather() {
       let weatherData = {
       city: "New York",
@@ -15,25 +17,7 @@ import "./App.css";
    
       return (
       <div className="Weather">
-         <form className="mb-3">
-            <div className="row">
-            <div className="col-9">
-               <input
-                  type="search"
-                  placeholder="Type a city.."
-                  className="form-control"
-                  autoComplete="off"
-               />
-            </div>
-            <div className="col-3">
-               <input
-                  type="submit"
-                  value="Search"
-                  className="btn btn-primary w-100"
-               />
-            </div>
-            </div>
-         </form>
+         <Search />
          <div className="overview">
             <h1>{weatherData.city}</h1>
             <ul>
